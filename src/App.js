5ad1758a026dfcae780c3d4ea31b13a5586c6d00
Umurzakov0204 <tomthompson0222@gmail.com>
2022-06-11 11:46:24 +0500

    count has been finished (i use useState)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+    const [number, setNumber] = useState(0);
+    // initial state => начальное состояние
+    // use state => состояние использования
+    // console.log(numberState);
+    // numberState -> array <{ const numberState: [number, Dispatch<SetStateAction<number>>] }>
+
+    function increment() {
+        setNumber((previous) => {
+            return  previous + 1
+        });
+        setNumber((previous) => {
+            return  previous + 1
+        });
+        // ^ две функции setNumber работают асинхронна "async" ^
+        // setNumber(number + 1)
+    }
+
+    function decriment() {
+        setNumber(number - 1)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="btn-group">
+          <h1>Номер: {number}</h1>
+        <button onClick={increment} className="success">Создать</button>
+        <button onClick={decriment} className="danger">Удалить</button>
+      </div>
     </div>
   );
 }
