ff66e84893ded8775460674d32b007e4beed4778
Umurzakov0204 <tomthompson0222@gmail.com>
2022-06-27 23:36:41 +0500

    router

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import React, {useState} from "react";
 import './App.css';
 import Props from "./components/Props/Props";
-import Comp from "./components/Comp";
-import State from "./components/Hooks/State/State";
-import Effects from "./components/Hooks/Effect/Effects";
+import {Link} from "react-router-dom"
 
 function App() {
-    const [number, setNumber] = useState(0);
-    // initial state => начальное состояние
-    // use state => состояние использования
-    // console.log(numberState);
-    // numberState -> array <{ const numberState: [number, Dispatch<SetStateAction<number>>] }>
-
-    function increment() {
-        setNumber((previous) => {
-            return previous + 1
-        });
-        setNumber((previous) => {
-            return previous + 1
-        });
-        // ^ две функции setNumber работают асинхронна "async" ^
-        // setNumber(number + 1)
-    }
-
-    function decriment() {
-        setNumber(number - 1)
-    }
+    // const [number, setNumber] = useState(0);
+    // // initial state => начальное состояние
+    // // use state => состояние использования
+    // // console.log(numberState);
+    // // numberState -> array <{ const numberState: [number, Dispatch<SetStateAction<number>>] }>
+    //
+    // function increment() {
+    //     setNumber((previous) => {
+    //         return previous + 1
+    //     });
+    //     setNumber((previous) => {
+    //         return previous + 1
+    //     });
+    //     // ^ две функции setNumber работают асинхронна "async" ^
+    //     // setNumber(number + 1)
+    // }
+    //
+    // function decriment() {
+    //     setNumber(number - 1)
+    // }
 
     return (
         <>
-            <div className="container">
-                <div className="btn-group">
-                    <h1>Номер: {number}</h1>
-                    <button onClick={increment} className="success">Создать</button>
-                    <button onClick={decriment} className="danger">Удалить</button>
-                </div>
-            </div>
+            <header>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="props">Props</Link>
+                    <Link to="effects">Effects</Link>
+                    <Link to="state">State</Link>
+                </nav>
+            </header>
+            {/*<div className="container">*/}
+            {/*    <div className="btn-group">*/}
+            {/*        <h1>Номер: {number}</h1>*/}
+            {/*        <button onClick={increment} className="success">Создать</button>*/}
+            {/*        <button onClick={decriment} className="danger">Удалить</button>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
             <Props name="Thomas" age="20" />
-            <Comp />
-            <State />
-            <Effects />
         </>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Props from "./components/Props/Props";
+import Effects from "./components/Hooks/Effect/Effects";
+import State from "./components/Hooks/State/State";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App/>}>
+                <Route path="props" element={<Props/>}>
+                    <Route path="effects" element={<Effects/>}/>
+                    <Route path="state" element={<State/>}/>
+                </Route>
+            </Route>
+        </Routes>
+    </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
